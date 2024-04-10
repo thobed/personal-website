1c0f7a16dda99a293f539240e252bba693697a27
thobed <dthobe2931@gmail.com>
2024-04-10 09:07:48 -0400

    test

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,22 +1,22 @@
 <header>
-    <nav class="navbar">
-        <h1>Darrell Thobe |</h1>
-        <ul>
-            <li>
-                <a href="https://github.com/thobed"
-                    ><img class="github" src="github-mark.png" /></a
-                >
-            </li>
-            <li>
-                <a href="https://www.linkedin.com/in/dthobe/"
-                    ><img class="linkedin" src="LI-In-Bug.png" /></a
-                >
-            </li>
-            <li>
-                <a href="https://www.facebook.com/darrell.thobe"
-                    ><img class="facebook" src="facebook_icon.png" /></a
-                >
-            </li>
-        </ul>
-    </nav>
+  <nav class="navbar">
+    <h1>Darrell Thobe |</h1>
+    <ul>
+      <li>
+        <a href="https://github.com/thobed"
+          ><img class="github" src="github-mark.png" /></a
+        >
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/dthobe/"
+          ><img class="linkedin" src="LI-In-Bug.png" /></a
+        >
+      </li>
+      <li>
+        <a href="https://www.facebook.com/darrell.thobe"
+          ><img class="facebook" src="facebook_icon.png" /></a
+        >
+      </li>
+    </ul>
+  </nav>
 </header>
